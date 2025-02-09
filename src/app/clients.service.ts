c34ee74f4f1dfff3100a302fd4502bf27a857c93
Luan Guedes <lcrisley98@gmail.com>
2025-02-08 21:58:41 -0300

    FEAT: Client List Component created

--- src/app/clients.service.ts
+++ src/app/clients.service.ts
@@ -13,4 +13,18 @@ export class ClientsService {
   save(client: Client) : Observable<Client> {
       return this.http.post<Client>('http://localhost:8080/api/clients', client)
   }
+
+  getClients() : Client[] {
+    let client = new Client();
+    client.id = 1
+    client.name = "Jose";
+    client.cpf = "07670233150"
+    client.registerDate = "20/01/2024"
+
+    return [client]
+  }
+
+  //getClients() : Observable<Client[]> {
+    //  return null;
+  //}
 }
---
+++ src/app/clients/clients-list/clients-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Client } from '../client';
+import { ClientsService } from 'src/app/clients.service';
+
+@Component({
+  selector: 'app-clients-list',
+  templateUrl: './clients-list.component.html',
+  styleUrls: ['./clients-list.component.css']
+})
+export class ClientsListComponent implements OnInit {
+
+  clients: Client[] = [];
+
+  constructor(private service: ClientsService) { }
+
+  ngOnInit(): void {
+      this.clients = this.service.getClients();
+  }
+
+}
